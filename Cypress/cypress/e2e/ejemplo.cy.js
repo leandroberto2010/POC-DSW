c754fd7f8814d967348de0776ed324878e764e69
leandroberto2010 <132699832+leandroberto2010@users.noreply.github.com>
2023-10-27 16:31:30 -0300

    Delete Cypress directory

--- Cypress/cypress/e2e/ejemplo.cy.js
+++
@@ -1,8 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.demoblaze.com/index.html#carouselExampleIndicators')
-    cy.contains("Samsung galaxy s6").click();
-    cy.get("a[href*='prod.html?idp_=1']").eq(1).click();
-
-  })
-})
--- Cypress/cypress/e2e/errorSintaxis.cy.js
+++
@@ -1,9 +0,0 @@
-describe('error de sintaxis por el encadenamiento', () => {
-  it('passes', () => {
-    cy.visit('https://www.demoblaze.com/index.html#carouselExampleIndicators')
-    //cy.visit('https://example.cypress.io')
-    //cy.screenshot('imgaen1')
-    cy.get("a").contains("Samsung galaxy s6").debug(); //ERROR DE SINTAXIS
-    
-  })
-})
